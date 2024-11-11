f101c5a2be071811a9361a9977f2b01bd768bced
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-11 11:44:15 +0200

    delete wine controller

--- controllers/wines.js
+++ controllers/wines.js
@@ -23,15 +23,19 @@ winesRouter.post('/wines', async (req, res) => {
         const query = 'INSERT INTO wines(code, brand, location, id, year, price) values($1,$2,$3,$4,$5,$6) RETURNING *'
         const values = [code, brand, location, id, year, price]
 
-        const wine = await pools.query(query,values)
+        const wine = await pools.query(query, values)
 
         res.json(wine.rows)
         console.log("New wine added")
 
     } catch (err) {
-        res.status(500).json({message:"wine could not be added"})
+        res.status(500).json({ message: "wine could not be added" })
         console.error(err)
     }
+})
+
+winesRouter.delete('/wines', async (req, res) => {
+    
 })
 
 module.exports = winesRouter
