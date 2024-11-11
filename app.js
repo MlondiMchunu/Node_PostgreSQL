173bdf7ac2666254dc9f73a53a9b3e917c1361cf
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-11 08:54:27 +0200

    wines router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const userRouter = require('./controllers/users')
+const winesRouter = require('./controllers/wines')
 
 const app = express()
 
@@ -9,5 +10,6 @@ app.use(morgan('dev'))
 
 app.use('/',userRouter);
 app.use('/users',userRouter)
+app.use('/get_wines',winesRouter)
 
 module.exports = app
--- controllers/wines.js
+++ controllers/wines.js
@@ -2,3 +2,16 @@ const winesRouter = require('express').Router()
 const pools = require('../models/db')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+
+winesRouter.get('/get_wines', async (req, res) => {
+    try {
+
+        const query = 'SELECT * FROM wines'
+        const wines = await pools.query(query)
+        res.json(wines)
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
