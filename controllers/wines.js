7768767a7b40d2b9778bdf72634568dc672c6999
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-11 11:29:22 +0200

    post new wine request

--- controllers/wines.js
+++ controllers/wines.js
@@ -16,4 +16,22 @@ winesRouter.get('/get_wines', async (req, res) => {
     }
 })
 
+winesRouter.post('/wines', async (req, res) => {
+    try {
+
+        const { code, brand, location, id, year, price } = req.body
+        const query = 'INSERT INTO wines(code, brand, location, id, year, price) values($1,$2,$3,$4,$5,$6) RETURNING *'
+        const values = [code, brand, location, id, year, price]
+
+        const wine = await pools.query(query,values)
+
+        res.json(wine.rows)
+        console.log("New wine added")
+
+    } catch (err) {
+        res.status(500).json({message:"wine could not be added"})
+        console.error(err)
+    }
+})
+
 module.exports = winesRouter
